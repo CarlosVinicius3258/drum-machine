f404a1667c2438bd58afa64a69cb4a6deab1be97
Carlos_Vinicius2340 <cvs.silva@discente.ufma.br>
2023-08-01 12:05:36 -0300

    add:  When a .drum-pad is triggered, a string describing the associated audio clip is displayed as the inner text of the #display element (each string must be unique)

--- src/components/DrumPad/index.tsx
+++ src/components/DrumPad/index.tsx
@@ -9,6 +9,7 @@ interface DrumPadProps {
 }
 import { useEffect, useRef, useState } from 'react';
 import Cev_H2 from '../../assets/audios/Cev_H2.mp3';
+import Cev_H3 from '../../assets/audios/Cev_H3.mp3';
 import Dsc_Oh from '../../assets/audios/Dsc_Oh.mp3';
 import Heater_1 from '../../assets/audios/Heater-1.mp3';
 import Heater_2 from '../../assets/audios/Heater-2.mp3';
@@ -19,7 +20,7 @@ import RP4_KICK_1 from '../../assets/audios/RP4_KICK_1.mp3';
 
 
 export default function DrumPad(props: DrumPadProps) {
-  const [lastPressedKey, setLastPressedKey] = useState<string>('');
+  const [isAudioPlaying, setIsAudioPlaying] = useState<boolean>(false);
   const audioRef = useRef<HTMLAudioElement | null>(null);
   const audios = [
     { keyPad: 'Q', src: Cev_H2 },
@@ -30,11 +31,17 @@ export default function DrumPad(props: DrumPadProps) {
     { keyPad: 'D', src: Heater_4 },
     { keyPad: 'Z', src: Kick_n_Hat },
     { keyPad: 'X', src: RP4_KICK_1 },
-    { keyPad: 'C', src: Cev_H2 }
+    { keyPad: 'C', src: Cev_H3 }
   ];
   const { keyPad, color, keyColor, className } = props;
   async function onClickPad() {
+    setIsAudioPlaying(true);
     await audioRef.current!.play();
+    setTimeout(() => {
+      setIsAudioPlaying(false);
+    }, 500);
+
+
   }
   const handleKeyDown = async (event: { key: string; }) => {
     // Play the audio whenever a key is pressed
@@ -50,10 +57,22 @@ export default function DrumPad(props: DrumPadProps) {
     return () => {
       document.removeEventListener('keydown', handleKeyDown);
     };
+
   }, []);
 
-  return <div onClick={ () => (async () => await onClickPad())() } className={ `drum-pad ${color ? color : ''} ${keyColor ? keyColor : ''} ${className ? className : ''}` }>
-    { keyPad }
+  useEffect(() => {
+    console.log('isAudioPlaying', isAudioPlaying);
+  }, [isAudioPlaying]);
+
+  function getSoundName(src: string) {
+    return src.split('/')[src.split('/').length - 1].split('.')[0];
+  }
+
+  return <div onClick={ () => (async () => await onClickPad())() } id={ getSoundName(audios.filter(aud => aud.keyPad === keyPad)[0]?.src) } className={ `drum-pad ${color ? color : ''} ${keyColor ? keyColor : ''} ${className ? className : ''}` }>
+    <p id='display'>
+      { !isAudioPlaying ? keyPad : getSoundName(audios.filter(aud => aud.keyPad === keyPad)[0]?.src) }
+    </p>
+
     {/* control play state */ }
 
     <audio ref={ audioRef } src={ audios.filter(aud => aud.keyPad === keyPad)[0]?.src } className='clip' id={ keyPad }></audio>
